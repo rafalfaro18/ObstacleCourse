39b515381cdf7bdc68e8e4b2bacb877e5e38d385
Rafael Alfaro Ramirez <r.alfaro.ramirez@accenture.com>
2022-05-06 00:27:05 -0600

    tags obstacles as Hit on colliding with Player

--- Assets/Scripts/ObjectHit.cs
+++ Assets/Scripts/ObjectHit.cs
@@ -7,6 +7,7 @@ public class ObjectHit : MonoBehaviour
     private void OnCollisionEnter(Collision other) {
         if(other.gameObject.tag == "Player"){
             GetComponent<MeshRenderer>().material.color = Color.red;
+            gameObject.tag = "Hit";
         }
     }
 }
