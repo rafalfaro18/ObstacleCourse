0ebccfd8cf9206380c6c6deb1b5d50907656dfa3
Rafael Alfaro Ramirez <r.alfaro.ramirez@accenture.com>
2022-05-05 23:48:14 -0600

    adds snippets comment

--- Assets/Scorer.cs
+++ Assets/Scorer.cs
@@ -6,6 +6,7 @@ public class Scorer : MonoBehaviour
 {
     int hits = 0;
     
+    // Install VS Code Snippets Extension to get Autocomplete for this function.
     private void OnCollisionEnter(Collision other) {
         hits++;
         Debug.Log("You've bumped into a thing " + hits + " times");
